c96dfb3ad8a95e6f9c957a63ea97f89cd139207f
许圣峰 <kricsleo@163.com>
2023-04-06 15:38:55 +0800

    chore: playground

--- playground/src/components/ToggleTheme.vue
+++ playground/src/components/ToggleTheme.vue
@@ -7,7 +7,8 @@ const themes = Object.keys(themeImports)
   .map(filepath => filepath.match(/(?<=\/)[^\/]*(?=.css$)/)![0])
 
 const theme = ref(themes[0])
-toggleTheme(theme.value)
+
+watch(theme, () => toggleTheme(theme.value), { immediate: true })
 
 function toggleTheme(theme: string) {
   const id = 'hljs-theme'
@@ -33,9 +34,7 @@ function toggleTheme(theme: string) {
         v-for="the in themes"
         :key="the"
         v-slot="{ checked }"
-        :value="the"
-        @click="toggleTheme(the)"
-      >
+        :value="the" >
         <button shrink-0 p-15 b="1 gray" rounded-4 :class="{ 'bg-rose b-rose': checked }">
           {{ the }}
         </button>
