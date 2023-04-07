9ce7f74d12c1b226f32f1fb58769c1ea5575dd4f
许圣峰 <kricsleo@163.com>
2023-04-07 15:14:19 +0800

    chore: style

--- playground/src/components/Header.vue
+++ playground/src/components/Header.vue
@@ -3,11 +3,17 @@ import { toggleDark } from '~/composables/dark'
 </script>
 
 <template>
-  <nav flex items-center justify-center sticky top-0 py-5 text-30 bg-white dark:bg-dark text-rose>
-    <h3>Preview themes</h3>
+  <nav flex items-center justify-center sticky top-0 py-5 text-30 bg-white dark:bg-dark>
+    <h3 text-rose text-center>
+      Preview Highlightjs Themes
+      <p text-14>
+        (Generated from VS Code themes)
+      </p>
+    </h3>
     <button
       i-carbon-sun dark:i-carbon-moon
       hover:op-80 transition-opacity absolute right-10
-      @click="toggleDark()" />
+      @click="toggleDark()"
+    />
   </nav>
 </template>
